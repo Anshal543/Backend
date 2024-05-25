847394f6fee76b6ba80834dcec97ecad0befe97b
Anshal543 <sherhasan543@gmail.com>
2024-05-25 19:16:46 +0500

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { ApiError } from '../utils/apiError.js';
 import { User } from '../models/user.model.js';
+import { uploadOnCloudinary } from '../utils/cloudinary.js';
+import { ApiResponse } from '../utils/apiResponse.js';
 
 
 const registerUser = asyncHandler(async (req, res, next) => {
@@ -23,8 +25,38 @@ const registerUser = asyncHandler(async (req, res, next) => {
     const existedUser = User.findOne({
         $or: [{ email }, { username }]
     })
+    if(existedUser) {
+        throw new ApiError(409, "User already exists");
+    }
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is required");
+    }
 
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(!avatar){
+        throw new ApiError(500, "avatar file is not uploaded");
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username:username.toLowerCase()
+    })
+
+    const createUser = await User.findById(user._id).select("-password -refreshToken");
+
+    if(!createUser){
+        throw new ApiError(500, "User is not created");
+    }
+    res.status(201).json(new ApiResponse(201, createUser, "User created successfully"));
 });
 
 export { registerUser };
