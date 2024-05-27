1d7cdf3c5d0b3108c24ddc5c374bea097a4e81f9
Anshal543 <sherhasan543@gmail.com>
2024-05-27 19:42:51 +0500

    user auth

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,23 @@ import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/apiResponse.js';
 
 
-const registerUser = asyncHandler(async (req, res, next) => {
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500, "Token generation failed")
+    }
+}
+
+const registerUser = asyncHandler(async (req, res) => {
     // get user detail from frontend
     // validation of user detail - not empty
     // check if user already exists : email, username
@@ -26,25 +42,25 @@ const registerUser = asyncHandler(async (req, res, next) => {
     const existedUser = await User.findOne({
         $or: [{ email }, { username }]
     })
-    if(existedUser) {
+    if (existedUser) {
         throw new ApiError(409, "User already exists");
     }
     const avatarLocalPath = req.files?.avatar[0]?.path;
     // console.log(req.files);
     // const coverImageLocalPath = req.files?.coverImage[0]?.path;
     let coverImageLocalPath;
-    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path;
     }
 
-    if(!avatarLocalPath){
+    if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required");
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath);
     const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
-    if(!avatar){
+    if (!avatar) {
         throw new ApiError(500, "avatar file is not uploaded");
     }
 
@@ -54,15 +70,67 @@ const registerUser = asyncHandler(async (req, res, next) => {
         coverImage: coverImage?.url || "",
         email,
         password,
-        username:username.toLowerCase()
+        username: username.toLowerCase()
     })
 
     const createUser = await User.findById(user._id).select("-password -refreshToken");
 
-    if(!createUser){
+    if (!createUser) {
         throw new ApiError(500, "User is not created");
     }
     res.status(201).json(new ApiResponse(201, createUser, "User created successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+    // req.body -> data
+    // username or email
+    // find the user
+    // check for password
+    // access and refresh token
+    // send cookies and response
+
+    const { username, email, password } = req.body;
+
+    if (!username || !email) {
+        throw new ApiError(400, "Username or email is required");
+    }
+
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid credentials");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                { user: loggedInUser, accessToken, refreshToken },
+                "User logged in successfully"))
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+
+})
+
+export { registerUser, loginUser };
