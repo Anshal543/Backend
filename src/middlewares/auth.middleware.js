557441a2bb1c49895a64beeb1ddcdee420fc6079
Anshal543 <sherhasan543@gmail.com>
2024-06-24 22:55:58 +0500

    .

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,14 @@
+import { ApiError } from "../utils/apiError";
 import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
 
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    if(!token) {
+        throw new ApiError(401, "Unauthorized")
+    }
+    const decode = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
 })
