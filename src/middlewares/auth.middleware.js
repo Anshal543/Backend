e1820f8e6f7b351b003651fe1fdf3caa0e453788
Anshal543 <sherhasan543@gmail.com>
2024-07-04 22:26:40 +0500

    middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,10 +5,21 @@ import { User } from "../models/user.model.js";
 
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-    if(!token) {
-        throw new ApiError(401, "Unauthorized")
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        if (!token) {
+            throw new ApiError(401, "Unauthorized")
+        }
+        const decode = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decode?.id).select("-password -refreshToken")
+        if (!user) {
+            throw new ApiError(401, "Unauthorized")
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        throw new ApiError(401, error.message || "Unauthorized")
     }
-    const decode = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    
-})
+
+
+}) 
